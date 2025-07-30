3bae35be208eaad63c4cdb65db7e77bd9cd01748
Riccardo Tornese <tuxnese@gmail.com>
2025-07-30 12:58:05 +0200

    Fix TF extrapolation error by setting LaserScan timestamp to scan start (header.stamp - scan_time)

--- src/scan_to_cloud_filter_chain.cpp
+++ src/scan_to_cloud_filter_chain.cpp
@@ -130,6 +130,9 @@ ScanToCloudFilterChain::scanCallback(
   sensor_msgs::msg::LaserScan filtered_scan;
   scan_filter_chain_.update(*scan_msg, filtered_scan);
 
+  // Adjust timestamp to the beginning of the scan to avoid TF extrapolation errors
+  filtered_scan.header.stamp = filtered_scan.header.stamp - rclcpp::Duration::from_seconds(filtered_scan.scan_time);
+
   // Project laser into point cloud
   sensor_msgs::msg::PointCloud2 scan_cloud;
 
